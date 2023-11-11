4237c1d4bd3d4566b29a2075cfc1dcaa7bff691d
Lucas <thebiglikboss777@gmail.com>
2023-11-11 16:40:34 -0500

    parte de texto sobre el carrusel de imagenes lista

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ let cerrarMenu = document.querySelector("#cerrarM");
 let nav = document.querySelector("#nav");
 let principalContainer = document.querySelector("#principal-container")
 let mobileContainer = document.querySelector("#mobileContainer")
+let container = document.getElementById('container')
+let slider = document.getElementById('slider');
+let slides = document.getElementsByClassName('slide').length;
+let buttons = document.getElementsByClassName('btn');
+
+let currentPosition = 0;
+let currentMargin = 0;
+let slidesPerPage = 0;
+let slidesCount = slides - slidesPerPage;
+let containerWidth = container.offsetWidth;
+let prevKeyActive = false;
+let nextKeyActive = true;
 
 abrirMenu.addEventListener("click", () => {
     nav.classList.add("visible")
@@ -11,3 +23,73 @@ abrirMenu.addEventListener("click", () => {
 cerrarMenu.addEventListener("click", () =>{
     nav.classList.remove("visible")
 })
+
+
+window.addEventListener("resize", checkWidth);
+
+function checkWidth() {
+    containerWidth = container.offsetWidth;
+    setParams(containerWidth);
+}
+
+function setParams(w) {
+    if (w < 551) {
+        slidesPerPage = 1;
+    } else {
+        if (w < 901) {
+            slidesPerPage = 2;
+        } else {
+            if (w < 1101) {
+                slidesPerPage = 3;
+            } else {
+                slidesPerPage = 4;
+            }
+        }
+    }
+    slidesCount = slides - slidesPerPage;
+    if (currentPosition > slidesCount) {
+        currentPosition -= slidesPerPage;
+    };
+    currentMargin = - currentPosition * (100 / slidesPerPage);
+    slider.style.marginLeft = currentMargin + '%';
+    if (currentPosition > 0) {
+        buttons[0].classList.remove('inactive');
+    }
+    if (currentPosition < slidesCount) {
+        buttons[1].classList.remove('inactive');
+    }
+    if (currentPosition >= slidesCount) {
+        buttons[1].classList.add('inactive');
+    }
+}
+
+setParams();
+
+function slideRight() {
+    if (currentPosition != 0) {
+        slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
+        currentMargin += (100 / slidesPerPage);
+        currentPosition--;
+    };
+    if (currentPosition === 0) {
+        buttons[0].classList.add('inactive');
+    }
+    if (currentPosition < slidesCount) {
+        buttons[1].classList.remove('inactive');
+    }
+};
+
+function slideLeft() {
+    if (currentPosition != slidesCount) {
+        slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
+        currentMargin -= (100 / slidesPerPage);
+        currentPosition++;
+    };
+    if (currentPosition == slidesCount) {
+        buttons[1].classList.add('inactive');
+    }
+    if (currentPosition > 0) {
+        buttons[0].classList.remove('inactive');
+    }
+};
+  
