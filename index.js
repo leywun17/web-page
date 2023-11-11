87c97c8a348bace1427b04ae2f23728ad425618e
Lucas <thebiglikboss777@gmail.com>
2023-11-11 18:24:18 -0500

    menu arreglado :(

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+let abrirMenu = document.querySelector("#abrir");
+let cerrarMenu = document.querySelector("#cerrarM");
+let nav = document.querySelector("#nav");
+let principalContainer = document.querySelector("#principal-container")
+let mobileContainer = document.querySelector("#mobileContainer")
+
+abrirMenu.addEventListener("click", () => {
+	nav.classList.add("visible")
+})
+
+cerrarMenu.addEventListener("click", () =>{
+	nav.classList.remove("visible")
+})
 
 /* carousel */
-var container = document.getElementById('container')
-var slider = document.getElementById('slider');
-var slides = document.getElementsByClassName('slide').length;
-var buttons = document.getElementsByClassName('btn');
+let container = document.getElementById('container')
+let slider = document.getElementById('slider');
+let slides = document.getElementsByClassName('slide').length;
+let buttons = document.getElementsByClassName('btn');
 
-var currentPosition = 0;
-var currentMargin = 0;
-var slidesPerPage = 0;
-var slidesCount = slides - slidesPerPage;
-var containerWidth = container.offsetWidth;
-var prevKeyActive = false;
-var nextKeyActive = true;
+let currentPosition = 0;
+let currentMargin = 0;
+let slidesPerPage = 0;
+let slidesCount = slides - slidesPerPage;
+let containerWidth = container.offsetWidth;
+let prevKeyActive = false;
+let nextKeyActive = true;
 
 window.addEventListener("resize", checkWidth);
 
@@ -80,98 +93,4 @@ function slideLeft() {
 		buttons[0].classList.remove('inactive');
 	}
 };
-let abrirMenu = document.querySelector("#abrir");
-let cerrarMenu = document.querySelector("#cerrarM");
-let nav = document.querySelector("#nav");
-let principalContainer = document.querySelector("#principal-container")
-let mobileContainer = document.querySelector("#mobileContainer")
-let container = document.getElementById('container')
-let slider = document.getElementById('slider');
-let slides = document.getElementsByClassName('slide').length;
-let buttons = document.getElementsByClassName('btn');
 
-let currentPosition = 0;
-let currentMargin = 0;
-let slidesPerPage = 0;
-let slidesCount = slides - slidesPerPage;
-let containerWidth = container.offsetWidth;
-let prevKeyActive = false;
-let nextKeyActive = true;
-
-abrirMenu.addEventListener("click", () => {
-    nav.classList.add("visible")
-})
-
-cerrarMenu.addEventListener("click", () =>{
-    nav.classList.remove("visible")
-})
-
-
-
-window.addEventListener("resize", checkWidth);
-
-function checkWidth() {
-    containerWidth = container.offsetWidth;
-    setParams(containerWidth);
-}
-
-function setParams(w) {
-    if (w < 551) {
-        slidesPerPage = 1;
-    } else {
-        if (w < 901) {
-            slidesPerPage = 2;
-        } else {
-            if (w < 1101) {
-                slidesPerPage = 3;
-            } else {
-                slidesPerPage = 4;
-            }
-        }
-    }
-    slidesCount = slides - slidesPerPage;
-    if (currentPosition > slidesCount) {
-        currentPosition -= slidesPerPage;
-    };
-    currentMargin = - currentPosition * (100 / slidesPerPage);
-    slider.style.marginLeft = currentMargin + '%';
-    if (currentPosition > 0) {
-        buttons[0].classList.remove('inactive');
-    }
-    if (currentPosition < slidesCount) {
-        buttons[1].classList.remove('inactive');
-    }
-    if (currentPosition >= slidesCount) {
-        buttons[1].classList.add('inactive');
-    }
-}
-
-setParams();
-
-function slideRight() {
-    if (currentPosition != 0) {
-        slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
-        currentMargin += (100 / slidesPerPage);
-        currentPosition--;
-    };
-    if (currentPosition === 0) {
-        buttons[0].classList.add('inactive');
-    }
-    if (currentPosition < slidesCount) {
-        buttons[1].classList.remove('inactive');
-    }
-};
-
-function slideLeft() {
-    if (currentPosition != slidesCount) {
-        slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
-        currentMargin -= (100 / slidesPerPage);
-        currentPosition++;
-    };
-    if (currentPosition == slidesCount) {
-        buttons[1].classList.add('inactive');
-    }
-    if (currentPosition > 0) {
-        buttons[0].classList.remove('inactive');
-	}
-};
